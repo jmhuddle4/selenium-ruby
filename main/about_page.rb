b924855325fe65898ab6a0e0e37f06283814a3c1
jmhuddle4 <jmhuddle4@gmail.com>
2018-04-16 02:00:56 -0400

    add csv function

--- main/about_page.rb
+++ main/about_page.rb
@@ -1,33 +1,43 @@
+require_relative 'base_page'
+
 class AboutPage
 
-  require_relative 'base_page'
-
-  def initialize
-   @base_page = BasePage.new
+  def initialize(base_page)
+    @base_page = base_page
   end
 
-  def fetch_staff_name(index)
-    @base_page.fetch_text_at_index(index, '.bio_wrapper h3')
+  def click_team_member(index)
+    @base_page.wait_for_seconds(0.5)
+    @base_page.click_element_at_index(index, '.bio_link')
   end
 
-  def fetch_staff_title(index)
-    @base_page.fetch_text_at_index(index, '.bio_wrapper span')
+  def close_bio_view
+    @base_page.wait_for_seconds(0.5)
+    @base_page.click('#bio_view .close_btn')
   end
 
-  def fetch_staff_size
-    @base_page.fetch_size_of_list('.bio_wrapper img')
+  def fetch_member_description
+    @base_page.fetch_text('#bio_view p')
   end
 
-  def navigate_to_page
+  def fetch_member_name
+    @base_page.fetch_text('#bio_view h3')
+  end
+
+  def fetch_member_title
+    @base_page.fetch_text('#bio_view .span3of5 span')
+  end
+
+  def fetch_num_team_members
+    @base_page.fetch_size_of_list('.bio_wrapper h3')
+  end
+
+  def navigate_to_about_page
     @base_page.navigate_to_page('about')
   end
 
   def staff_section_displays
-    @base_page.element_is_displayed('bio_wrapper')
-  end
-
-  def stop
-    @base_page.stop
+    @base_page.element_is_displayed('.bio_wrapper')
   end
 
 end
--- test/about_page_tests.rb
+++ test/about_page_tests.rb
@@ -1,23 +1,49 @@
 class AboutPageTests
   require_relative '../main/about_page'
+  require_relative '../main/base_page'
+  require_relative '../main/home_page'
+  require 'csv'
   require 'test/unit'
   extend Test::Unit::Assertions
 
-  a = AboutPage.new
-  a.navigate_to_page
-  assert a.staff_section_displays
+  # start driver with the base page
+  base_page = BasePage.new
 
-  num_staff_members = a.fetch_staff_size
+  # verify navigation to home page
+  home_page = HomePage.new(base_page)
+  home_page.navigate_to_home_page
+  assert home_page.home_page_title_displays
 
-  for counter in 0..num_staff_members - 1
-    name = a.fetch_staff_name(counter)
-    title = a.fetch_staff_title(counter)
+  # verify about page navigation
+  about_page = AboutPage.new(base_page)
+  about_page.navigate_to_about_page
+  assert about_page.staff_section_displays
 
-    puts 'name: ' + name
-    puts 'title: ' + title
+  # get number of team member cards
+  num_team_members = about_page.fetch_num_team_members
+
+  # create/overwrite csv file with all team member names, titles & descriptions
+  CSV.open('team_memembers.csv', 'wb') do |csv|
+    csv << ['Name', 'Title', 'Description']
+
+    for counter in 0..num_team_members - 1
+      about_page.click_team_member(counter)
+      name = about_page.fetch_member_name
+      title = about_page.fetch_member_title
+      description = about_page.fetch_member_description
+
+      puts 'name: ' + name
+      puts 'title: ' + title
+      puts 'desc: ' + description
+
+      csv << [name, title, description]
+
+      about_page.close_bio_view
+    end
 
   end
 
-  a.stop
+  # stop driver
+  base_page.stop_driver
 
 end
